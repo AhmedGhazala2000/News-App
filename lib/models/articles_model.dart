33496ddf0953886a4090547eff81fa050a29ae9e
AhmedGhazala2000 <ahmed.ghazala142000@gmail.com>
2024-02-02 21:33:57 +0200

    Added url launcher to articles

--- lib/models/articles_model.dart
+++ lib/models/articles_model.dart
@@ -2,14 +2,15 @@ class ArticlesModel {
   final String? image;
   final String title;
   final String? description;
+  final String url;
 
   ArticlesModel(
-      {required this.image, required this.title, required this.description});
+      {required this.image, required this.title, required this.description,required this.url});
 
   factory ArticlesModel.fromJson({required jsonData}) {
     return ArticlesModel(
         image: jsonData['urlToImage'],
         title: jsonData['title'],
-        description: jsonData['description']);
+        description: jsonData['description'], url: jsonData['url']);
   }
 }
--- lib/widgets/news_tile.dart
+++ lib/widgets/news_tile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/models/articles_model.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class NewsTile extends StatelessWidget {
   const NewsTile({super.key, required this.article});
@@ -8,42 +9,47 @@ class NewsTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 20),
-      child: Column(
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(8),
-            child: Image.network(
-              article.image ??
-                  'https://presstetouan.mcdn.ma/static/uploads/2020/02/News.jpg',
-              height: 250,
-              width: double.infinity,
-              fit: BoxFit.cover,
+    return GestureDetector(
+      onTap: () {
+        launchUrl(Uri.parse(article.url));
+      },
+      child: Padding(
+        padding: const EdgeInsets.only(bottom: 20),
+        child: Column(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(8),
+              child: Image.network(
+                article.image ??
+                    'https://presstetouan.mcdn.ma/static/uploads/2020/02/News.jpg',
+                height: 250,
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
             ),
-          ),
-          const SizedBox(
-            height: 5,
-          ),
-          Text(
-            article.title,
-            maxLines: 2,
-            overflow: TextOverflow.ellipsis,
-            style: const TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.w500,
+            const SizedBox(
+              height: 5,
             ),
-          ),
-          Text(
-            article.description ?? '',
-            maxLines: 3,
-            overflow: TextOverflow.ellipsis,
-            style: const TextStyle(
-              fontSize: 16,
-              color: Colors.grey,
+            Text(
+              article.title,
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              style: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w500,
+              ),
             ),
-          ),
-        ],
+            Text(
+              article.description ?? '',
+              maxLines: 3,
+              overflow: TextOverflow.ellipsis,
+              style: const TextStyle(
+                fontSize: 16,
+                color: Colors.grey,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
