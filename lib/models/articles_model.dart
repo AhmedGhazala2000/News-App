076cf56643310e097a4cb72a1c72302aa89d9443
AhmedGhazala2000 <ahmed.ghazala142000@gmail.com>
2024-02-04 00:22:09 +0200

    Added url launcher package

--- lib/models/articles_model.dart
+++ lib/models/articles_model.dart
@@ -5,12 +5,17 @@ class ArticlesModel {
   final String url;
 
   ArticlesModel(
-      {required this.image, required this.title, required this.description,required this.url});
+      {required this.image,
+      required this.title,
+      required this.description,
+      required this.url});
 
   factory ArticlesModel.fromJson({required jsonData}) {
     return ArticlesModel(
-        image: jsonData['urlToImage'],
-        title: jsonData['title'],
-        description: jsonData['description'], url: jsonData['url']);
+      image: jsonData['urlToImage'],
+      title: jsonData['title'],
+      description: jsonData['description'],
+      url: jsonData['url'],
+    );
   }
 }
