b062177fb2aa1880056dda3e79d89d8177e3b64e
AhmedGhazala2000 <ahmed.ghazala142000@gmail.com>
2024-03-20 00:52:08 +0200

    Update app icon and name

--- lib/constant.dart
+++ lib/constant.dart
@@ -4,17 +4,20 @@ const double smallDeviceSize = 900;
 
 const List<CategoryModel> categories = [
   CategoryModel(
-      categoryAssetImage: 'assets/business.jpg', categoryName: 'Business'),
+      categoryAssetImage: 'assets/images/business.jpg',
+      categoryName: 'Business'),
   CategoryModel(
-      categoryAssetImage: 'assets/science.webp', categoryName: 'Science'),
+      categoryAssetImage: 'assets/images/science.webp',
+      categoryName: 'Science'),
   CategoryModel(
-      categoryAssetImage: 'assets/technology.jpg', categoryName: 'Technology'),
+      categoryAssetImage: 'assets/images/technology.jpg',
+      categoryName: 'Technology'),
   CategoryModel(
-      categoryAssetImage: 'assets/sports.webp', categoryName: 'Sports'),
+      categoryAssetImage: 'assets/images/sports.webp', categoryName: 'Sports'),
   CategoryModel(
-      categoryAssetImage: 'assets/health.jpg', categoryName: 'Health'),
+      categoryAssetImage: 'assets/images/health.jpg', categoryName: 'Health'),
   CategoryModel(
-      categoryAssetImage: 'assets/entertainment.png',
+      categoryAssetImage: 'assets/images/entertainment.png',
       categoryName: 'Entertainment'),
   CategoryModel(
       categoryAssetImage: 'assets/general.jpg', categoryName: 'General'),
